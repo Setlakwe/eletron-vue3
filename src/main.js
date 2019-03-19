1a9463b0b99ca6e740922333210c2b941252b487
Christian Baril <barilc@setlakwemode.com>
2019-03-19 15:13:23 -0400

    Added vuelidate dependencie

--- src/main.js
+++ src/main.js
@@ -2,9 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import BootstrapVue from 'bootstrap-vue'
-
+import Vuelidate from 'vuelidate'
 
 Vue.use(BootstrapVue)
+Vue.use(Vuelidate)
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
